1e5e2bd56234439754fa3b0281bdc48a70284f81
redheadcruzcruz-svg <redheadcruzcruz@gmail.com>
2026-03-27 17:57:48 -0400

    Dia #1: Empezamos hacer hoy la pagina del Login y luego el Dashboard para luego probar los primeros botones funcionales del programa en la web que hemos estado trabajando. A Jonathan le tocó hacer ambas paginas hoy y el  diseño de ambas para establecer un buen comienzo del proyecto. Proximamente, a Kiara le tocará hacer las paginas secundarias del Dashboard.

---
+++ app/dashboard.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Dashboard from '../app-example/components/Dashboard';
+
+export default function DashboardScreen() {
+  return <Dashboard />;
+}
---
+++ app/index.tsx
@@ -0,0 +1,5 @@
+import LoginPage from '../app-example/components/LoginPage';
+
+export default function Index() {
+  return <LoginPage />;
+}
